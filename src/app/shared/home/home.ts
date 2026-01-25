c204766f6f8c60b99b19c090423739b6a4fda36b
JEspinosa1770 <jespinosa1770@gmail.com>
2026-01-25 20:41:56 +0100

    feat(pages): add basic home and event list pages #2

--- src/app/shared/home/home.ts
+++ src/app/shared/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+  private router = inject(Router);
+
+  goToEventsList(): void {
+    this.router.navigate(['/list']);
+  }
 
 }
