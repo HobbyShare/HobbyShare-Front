05b0ea733204b86b20644abadd88c238cc7473b9
JEspinosa1770 <jespinosa1770@gmail.com>
2026-01-25 20:50:15 +0100

    feat(components): add basic events list and event form components #2

---
+++ src/app/events/events-list/events-list.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { EventsService } from '../../core/services/events.service';
+import { EventModel } from '../../core/modals/event-model';
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-events-list',
+  imports: [],
+  templateUrl: './events-list.html',
+  styleUrl: './events-list.css',
+})
+export class EventsList implements OnInit{
+  eventsService = inject(EventsService);
+  private router = inject(Router);
+
+  events = signal<EventModel[]>([]);
+  // currentPage = signal<number>(1);
+  // totalPages = signal<number>(0);
+  isLoading = signal<boolean>(false);
+  error = signal<string | null>(null);
+
+  ngOnInit(): void {
+    this.eventsService.loadEvents();
+  }
+}
