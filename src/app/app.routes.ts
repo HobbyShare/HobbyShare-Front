6bfcc092cabf58eeee94b3556eaf15e93e7e237d
JEspinosa1770 <jespinosa1770@gmail.com>
2026-01-25 19:10:09 +0100

    feat(routes): add initial routes #2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './shared/home/home';
+import { EventsList } from './events/events-list/events-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'list', component: EventsList }, // , canActivate: [authGuard]
+  // { path: 'events/:id', component: FilmDetail, canActivate: [authGuard] },
+  // { path: 'login', component: Login },
+  // { path: 'register', component: Register},
+  { path: "**", redirectTo: ''}
+];
