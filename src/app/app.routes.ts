2e9f5107490a960d32b3945ae89ec773431f9e1a
nereame96 <nereame96@gmail.com>
2026-02-02 17:53:10 +0100

    Feature/calendar (#2)
    * feat: authService implemented
    
    * feat: register and login implemented, missing style
    
    * feat: implement calendar
    
    * feature: fix merge issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,42 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home';
+import { LoginComponent } from './auth/login/login';
+import { RegisterComponent } from './auth/register/register';
+import { CalendarComponent } from './events/calendar/calendar';
 import { EventsList } from './events/events-list/events-list';
 import { EventForm } from './events/event-form/event-form';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+
+  {
+    path: '',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+
+
   { path: 'events', component: EventsList }, // , canActivate: [authGuard]
   { path: 'events/new', component: EventForm }, // , canActivate: [authGuard]
   { path: 'events/:id/edit', component: EventForm }, // , canActivate: [authGuard]
-  // { path: 'events/:id', component: FilmDetail, canActivate: [authGuard] },
-  // { path: 'login', component: Login },
-  // { path: 'register', component: Register},
-  { path: "**", redirectTo: ''}
+
+  {
+    path: 'calendar',
+    component: CalendarComponent,
+  },
+
+  {
+    path: '**',
+    redirectTo: '',
+  }
+
+
 ];
---
+++ src/app/auth/register/register.ts
@@ -0,0 +1,110 @@
+import { routes } from './../../app.routes';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
+import { Hobby } from '../../core/enums/hobby.enum';
+import { AuthService } from '../../core/services/auth.service';
+import { email } from '@angular/forms/signals';
+
+@Component({
+  selector: 'app-register',
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class RegisterComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService)
+  private router = inject(Router)
+  errorMessage = signal('');
+  successMessage = signal('');
+  isLoading = signal(false)
+
+  public readonly Hobby = Hobby
+
+  public hobbyList = Object.values(Hobby)
+
+  selectedHobbies = signal<string[]>([])
+
+
+  registerForm = this.fb.group({
+      userName: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    }
+  );
+
+  toggleHobby(hobby: string): void {
+    const current = this.selectedHobbies()
+    const index = current.indexOf(hobby)
+
+    if(index === -1) {
+      this.selectedHobbies.set([...current, hobby])
+    } else {
+      this.selectedHobbies.set(current.filter(h => h !== hobby))
+    }
+  }
+
+  isHobbySelected(hobby: string): boolean {
+    return this.selectedHobbies().includes(hobby)
+  }
+
+  onSubmit(): void {
+
+    this.errorMessage.set('')
+    this.successMessage.set('')
+
+    if(this.registerForm.invalid) {
+      this.errorMessage.set('Please, fulfill all the fields')
+      return
+    }
+
+    if(this.selectedHobbies().length === 0) {
+      this.errorMessage.set('Please, choose at least one hobby')
+      return
+    }
+
+    const password = this.registerForm.value.password
+    const confirmPassword = this.registerForm.value.confirmPassword
+
+    if(password !== confirmPassword) {
+      this.errorMessage.set('Passwords do not match')
+    }
+
+    const userDate = {
+      userName: this.registerForm.value.userName,
+      name: this.registerForm.value.name,
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password,
+      hobbies: this.selectedHobbies()
+    }
+
+    this.isLoading.set(true)
+
+    this.authService.register(userDate).subscribe({
+      next: (response) => {
+        this.isLoading.set(false);
+        this.successMessage.set('Successfully registered');
+
+        setTimeout(() => {
+          this.router.navigate(['/login'])
+        }, 2000);
+      },
+
+      error: (error) => {
+        this.isLoading.set(false)
+        this.errorMessage.set(error.error?.message || 'Error registering user')
+      }
+
+    })
+
+
+
+
+
+  }
+
+
+}
