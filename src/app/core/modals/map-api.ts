16fb763fec38c357c5caad2bafc1478914c8b282
JEspinosa1770 <jespinosa1770@gmail.com>
2026-02-04 22:30:14 +0100

    refactor(map): extract map services into a service file #3

--- src/app/core/modals/map-api.ts
+++ src/app/core/modals/map-api.ts
@@ -1,2 +1,16 @@
 export interface MapApi {
 }
+
+export interface MapConfig {
+  containerId: string;
+  center: [number, number];
+  zoom: number;
+}
+
+export interface MarkerConfig {
+  lat: number;
+  lng: number;
+  icon?: L.Icon;
+  draggable?: boolean;
+  popup?: string;
+}
