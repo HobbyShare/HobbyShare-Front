55e4161b778b891b651e5ff7e1f50be7895a8834
nereame96 <nereame96@gmail.com>
2026-02-05 17:47:02 +0100

    chore: create navbar component

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class NavbarComponent {
+
+  private authService = inject(AuthService)
+
+  isAuthenticated = this.authService.isAuthenticated
+  onLogout = this.authService.logout
+
+}
